ec84182ff3599cf9acf383db470ee5249f198351
MurphyWhite <mzh306291465@gmail.com>
2022-09-10 21:02:38 +0800

    init.el

--- init.el
+++ init.el
@@ -4,11 +4,16 @@
 ;; --------------------
 
 (require 'package)
+(package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+
+;; refresh the package list
+(package-refresh-contents)
+
+
 
 (global-linum-mode t) ;; enable line numbers globally
 
@@ -27,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-java)))
+ '(package-selected-packages '(php-mode markdown-mode lsp-java)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
